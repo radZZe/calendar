c147f4865413aa92977f1a02dc9fbea1a4f3a2c3
Danil <drazdobarov.job@yandex.ru>
2022-03-16 20:24:41 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css'
+import './assets/css/schema_dark.css'
+import './assets/css/animate.css'
+import './assets/css/bootstrap-select.min.css'
+import './assets/css/bootstrap.min.css'
+import './assets/css/line-awesome.min.css'
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer'
+import Calendar from './Components/Calendar/Calendar'
+import Button from './Components/Button/Button'
+import Title from './Components/Title/Title'
+import {useRef, useState, useEffect} from 'react'
+import {Routes, Route, Link} from 'react-router-dom';
+import classname from './assets/css/Navbar.module.css'
+import moment from 'moment'
+import Category from './Components/Category/Category'
+import AddCategory from './Components/Category/TypeOfCategory/Categories/NewCartCategory/AddCategory/AddCategory'
+import CreateCategory
+    from './Components/Category/TypeOfCategory/Categories/NewCartCategory/CreateCategory/CreateCategory'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    const startDay = moment().startOf('month').startOf('day').add(1, 'month')
+    console.log(startDay)
+    const [calendarFade, setCalendarFade] = useState(true)
+    const [activePage, setActivePage] = useState('Календарь')
+
+    return (
+        <div className="App">
+            <Header/>
+            <div className='App_wrapper'>
+                <Title text={activePage}/>
+                <div className={classname.content}>
+                    <div className={classname.links}>
+                        <Link onClick={() => setActivePage('Категории')} to='/category'>Категории</Link>
+                        <Link onClick={() => setActivePage('Календарь')} to='/'>Календарь</Link>
+                        <Link onClick={() => setActivePage('Цены')} to='/price'>Цены</Link>
+                    </div>
+                    <hr></hr>
+                </div>
+                <Routes>
+                    <Route path='/' element={<Calendar dispatch={props.dispatch} state={props.state} fade={calendarFade}
+                                                       setFade={setCalendarFade}/>}/>
+                    <Route path='/price'
+                           element={<Calendar price dispatch={props.dispatch} state={props.state} fade={calendarFade}
+                                              setFade={setCalendarFade}/>}/>
+                    <Route path='/category/*' element={<Category dispatch={props.dispatch} state={props.state}/>}/>
+                    <Route path='/create_category' element={<AddCategory dispatch={props.dispatch}/>}/>
+                    <Route path={`/edit_category`}
+                           element={<CreateCategory dispatch={props.dispatch} state={props.state}/>}/>
+                </Routes>
+            </div>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import store from './redux/redux-store.js'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App store={store} dispatch={store.dispatch.bind(store)} state={state} />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(()=>{
+  rerenderEntireTree(store.getState());
+});
